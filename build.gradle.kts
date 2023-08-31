71b2df69d7e2942107e2cd31cc13b3d26f9ce426
daniel <daniel@example.com>
2023-08-31 18:43:57 +0800

    Create empty spring boot project with gradle

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    java
+    id("org.springframework.boot") version "2.7.15"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+group = "com.crypto.dan"
+version = "1.0.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
